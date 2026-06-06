42b8c05a44fa55b063ed08c99ca310787300b201
Mayank <144101564+mk4727@users.noreply.github.com>
2026-06-06 13:41:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { MusicPlayer } from "@/components/MusicPlayer";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import Memories from "./pages/Memories.tsx";
@@ -26,11 +27,12 @@ const App = () => (
           <Route path="/messages" element={<Messages />} />
           <Route path="/games" element={<Games />} />
           <Route path="/scrapbook" element={<Scrapbook />} />
-          {/* <Route path="/flowers" element={<Flowers />} /> */}
+          <Route path="/flowers" element={<Flowers />} />
           <Route path="/admin" element={<Admin />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <MusicPlayer />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
